681b7c6f5ee8cd68bb0c70b84f0b1f64972642eb
CANMERTINYO <38213551+c4nzin@users.noreply.github.com>
2024-11-26 19:33:21 +0300

    update

--- src/interfaces/rate-limiter-options.interface.ts
+++ src/interfaces/rate-limiter-options.interface.ts
@@ -1,4 +1,12 @@
+/**
+ * Options for the rate limiter configuration.
+ *
+ * @property ms - The time window in milliseconds for rate limiting.
+ * @property maxRequest - The maximum number of allowed requests in the time window.
+ */
 export interface RateLimiterOptions {
+  /** The time in milliseconds for rate limiting. */
   ms: number;
+  /** The maximum number of allowed requests in the time window. */
   maxRequest: number;
 }
